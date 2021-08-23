2873ba7c8dec9caad59054b6a314b0794211ed41
lucas.nascimento.tahansab <lucas.nascimento@cbcloud.com.br>
2021-08-23 12:06:04 -0300

    Criação das abas laterais, abertura e fechamento

--- implementacao01/javascripts/index.js
+++ implementacao01/javascripts/index.js
@@ -4,9 +4,89 @@ const canvasController = new CanvasController
 
 window.addEventListener('load', () => {
   carregarCanvas()
+  document.getElementById('tranformacoes-geometricas').addEventListener('click', () => {
+    fecharGuiaLateral('home')
+    abrirGuiaLateral('transformacao-geometrica-opcoes')
+  })
+  document.getElementById('desenhar-reta').addEventListener('click', () => {
+    fecharGuiaLateral('home')
+    abrirGuiaLateral('desenhar-reta-opcoes')
+  })
+  document.getElementById('desenhar-circunferencia').addEventListener('click', () => {
+    fecharGuiaLateral('home')
+    abrirGuiaLateral('desenhar-circunferencia-dados')
+  })
+  document.getElementById('translacao').addEventListener('click', () => {
+    fecharGuiaLateral('transformacao-geometrica-opcoes')
+    abrirGuiaLateral('transformacao-geometrica-fatores')
+  })
+  document.getElementById('rotacao').addEventListener('click', () => {
+    fecharGuiaLateral('transformacao-geometrica-opcoes')
+    abrirGuiaLateral('transformacao-geometrica-fator-rotacao')
+  })
+  document.getElementById('escala').addEventListener('click', () => {
+    fecharGuiaLateral('transformacao-geometrica-opcoes')
+    abrirGuiaLateral('transformacao-geometrica-fatores')
+  })
+  document.getElementById('reflexao').addEventListener('click', () => {
+    fecharGuiaLateral('transformacao-geometrica-opcoes')
+    abrirGuiaLateral('transformacao-geometrica-fatores')
+  })
+  document.getElementById('dda').addEventListener('click', () => {
+    fecharGuiaLateral('desenhar-reta-opcoes')
+    abrirGuiaLateral('desenhar-reta-pontos')
+  })
+  document.getElementById('bresenham').addEventListener('click', () => {
+    fecharGuiaLateral('desenhar-reta-opcoes')
+    abrirGuiaLateral('desenhar-reta-pontos')
+  })
+  document.getElementById('transformacao-geometrica-opcoes-para-home').addEventListener('click', () => {
+    fecharGuiaLateral('transformacao-geometrica-opcoes')
+    abrirGuiaLateral('home')
+  })
+  document.getElementById('transformacao-geometrica-fatores-para-opcoes').addEventListener('click', () => {
+    fecharGuiaLateral('transformacao-geometrica-fatores')
+    abrirGuiaLateral('transformacao-geometrica-opcoes')
+  })
+  document.getElementById('transformacao-geometrica-fatores-para-home').addEventListener('click', () => {
+    fecharGuiaLateral('transformacao-geometrica-fatores')
+    abrirGuiaLateral('home')
+  })
+  document.getElementById('transformacao-geometrica-fator-rotacao-para-opcoes').addEventListener('click', () => {
+    fecharGuiaLateral('transformacao-geometrica-fator-rotacao')
+    abrirGuiaLateral('transformacao-geometrica-opcoes')
+  })
+  document.getElementById('transformacao-geometrica-fator-rotacao-para-home').addEventListener('click', () => {
+    fecharGuiaLateral('transformacao-geometrica-fator-rotacao')
+    abrirGuiaLateral('home')
+  })
+  document.getElementById('desenhar-reta-opcoes-para-home').addEventListener('click', () => {
+    fecharGuiaLateral('desenhar-reta-opcoes')
+    abrirGuiaLateral('home')
+  })
+  document.getElementById('desenhar-reta-pontos-para-opcoes').addEventListener('click', () => {
+    fecharGuiaLateral('desenhar-reta-pontos')
+    abrirGuiaLateral('desenhar-reta-opcoes')
+  })
+  document.getElementById('desenhar-reta-pontos-para-home').addEventListener('click', () => {
+    fecharGuiaLateral('desenhar-reta-pontos')
+    abrirGuiaLateral('home')
+  })
+  document.getElementById('desenhar-circunferencia-dados-para-home').addEventListener('click', () => {
+    fecharGuiaLateral('desenhar-circunferencia-dados')
+    abrirGuiaLateral('home')
+  })
 })
 
 function carregarCanvas() {
   canvasController.carregarCanvas()
   canvasController.carregarMalhaDePixels()
+}
+
+function fecharGuiaLateral(nomeGuia) {
+  document.getElementById(nomeGuia).classList.add('navbar--closed')
+}
+
+function abrirGuiaLateral(nomeGuia) {
+  document.getElementById(nomeGuia).classList.remove('navbar--closed')
 }
